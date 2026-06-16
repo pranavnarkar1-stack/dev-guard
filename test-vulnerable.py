240f984ff39663d93686239408ca1060c0a51db4
Pranav Narkar <pranav.narkar1@gmail.com>
2026-06-16 18:00:51 +0530

    Update test-vulnerable.py

--- test-vulnerable.py
+++ test-vulnerable.py
@@ -2,3 +2,4 @@ db_password = "supersecret123"
 api_key = "sk-abc123realkeyvalue"
 query = "SELECT * FROM users WHERE id = " + user_input
 print("Logging in with password:", db_password)
+password = "admin123"
